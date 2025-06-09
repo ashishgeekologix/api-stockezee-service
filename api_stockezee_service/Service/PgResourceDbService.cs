bb613939924d5bad9630575bd581e58852a86f9b
ashishgeekologix <ashish.geekologix@gmail.com>
2025-06-09 11:01:40 +0530

    orb-order-fixes

--- api_stockezee_service/Service/PgResourceDbService.cs
+++ api_stockezee_service/Service/PgResourceDbService.cs
@@ -669,10 +669,11 @@ from fao_data f left join cte_nifty as eq on f.created_at=eq.prev_date where cli
             try
             {
 
-                var sql = @"
-                                    WITH bullish AS (
+                var sql = @"                                    WITH bullish AS (
     SELECT 
-        rb.*, 
+        rb.symbol_name,
+		rb.time,
+		rb.current_score,
         'Bullish' AS trend,
         eq.last_trade_price,
         eq.change,
@@ -681,12 +682,14 @@ from fao_data f left join cte_nifty as eq on f.created_at=eq.prev_date where cli
     INNER JOIN nse_eq_stock_data_daily eq 
         ON eq.symbol_name = rb.symbol_name
     WHERE rb.current_score > 0
-    ORDER BY rb.current_score DESC
+    ORDER BY rb.current_score DESC,eq.change_percent DESC
     LIMIT 10
 ),
 bearish AS (
     SELECT 
-        rb.*, 
+        rb.symbol_name,
+		rb.time,
+		rb.current_score, 
         'Bearish' AS trend,
         eq.last_trade_price,
         eq.change,
@@ -695,7 +698,7 @@ bearish AS (
     INNER JOIN nse_eq_stock_data_daily eq 
         ON eq.symbol_name = rb.symbol_name
     WHERE rb.current_score < 0
-    ORDER BY rb.current_score ASC
+    ORDER BY rb.current_score ASC,eq.change_percent DESC
     LIMIT 10
 )
 SELECT * FROM bullish
